f24a59152a0bbe4993f5c54c31654942b5824dca
Manas Satish Bedmutha <manasbedmutha98@gmail.com>
2019-03-16 00:01:11 +0530

    Changed struct; added init

--- gradient_t.h
+++ gradient_t.h
@@ -5,10 +5,18 @@ struct gradient_t
 {
 	float grad;
 	float oldgrad;
-	gradient_t()
-	{
-		grad = 0;
-		oldgrad = 0;
-	};
+	// gradient_t()
+	// {
+	// 	grad = 0;
+	// 	oldgrad = 0;
+	// };
 	
 };
+
+struct gradient_t * initialize(int grad, int oldgrad){
+	struct gradient_t * gradtsr; 
+	//= malloc(sizeof(gradient_t));
+	gradtsr->grad = grad;
+	gradtsr->oldgrad = oldgrad;
+	return gradtsr;
+}
