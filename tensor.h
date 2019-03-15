381a6f69e0d525250c1253f585ef7beedd259990
pyrocoder <33415282+pyrocoder@users.noreply.github.com>
2019-03-15 14:47:32 +0530

    Add files via upload

--- tensor.h
+++ tensor.h
@@ -7,7 +7,7 @@ struct tensor( int _x, int _y, int _z )
 	{
 
 		tdsize size;
-		float* data;
+		float* data[_x*_y*_z];
 		size.x = _x;
 		size.y = _y;
 		size.z = _z;
@@ -16,60 +16,67 @@ struct tensor( int _x, int _y, int _z )
 
 //defining a method to input a tensor into another tensor
 
- struct tensor* init(x,y,z)	
+ struct tensor* init_tensor(x,y,z)	
+ {
+ 	struct tensor * t = malloc(sizeof(*input));
+	t->size.x = input->size.x;
+	t->size.y = input->size.y;
+	t->size.z = input->size.z;
+
+	return t;
+
+ }
  struct tensor * load_tensor( struct tensor * input)
 	{
-		struct tensor * t = malloc(sizeof(*input));
-		t->size.x = input-size.x;
-		t->size.y = input-size.y;
-		t->size.z = input-size.z;
+		struct tensor* t;
 
-
-
-
-
-		
+		t = init_tensor(input->size.x,input->size.y,input->size.z);
 		t->data = malloc((input->size.x * input->size.y * input->size.z *sizeof(float));
 
 	    for ( int i = 0; i < t.size.x * t.size.y * t.size.z; i++ )
 			{
 
+				*(t->data[i]) = *(input->data[i]);
 			}
-		t->size = other.size;
-
+		
+		return t
 	}
+	
 
 //Adding the the elements of the tensors
-   struct tensor add_tensor( struct tensor t, struct tensor other)
+   struct tensor add_tensor( struct tensor* t, struct tensor* other)
 	{	
-		struct tensor t_sum(t.size.x,t.size.y,t.size.z);
-		t_sum.data = malloc(t.size.x*t.size.x*t.size.x)
+		struct tensor* t_sum;
+
+		t_sum = init_tensor(input->size.x,input->size.y,input->size.z);
+		t_sum->data = malloc(t->size.x * t->size.x * t->size.x)
 		
 		for ( int i = 0; i < t.size.x * t.size.y * t.size.z; i++ )
-			t_sum.data[i] = t.data[i]+other.data[i];
+			*(t_sum->data[i]) = *(t->data[i])+*(other->data[i]);
 		return t_sum;
 	}	
 
 //subtracting the elements of t from other
-	struct tensor subtract_tensor( struct tensor t, struct tensor other)
+	struct tensor* subtract_tensor( struct tensor* t, struct tensor* other)
 	{	
-		struct tensor t_sum(t.size.x,t.size.y,t.size.z);
-		t_sum.data = malloc(t.size.x*t.size.x*t.size.x)
-		
-		for ( int i = 0; i < t.size.x * t.size.y * t.size.z; i++ )
-			t_sum.data[i] = t.data[i]-other.data[i];
-		return t_sum;
+		struct tensor* t_sub;
+		t_sub = init_tensor(t->size.x,t->size.y,t->size.z);
+		t_sub->data = malloc(t->size.x* t->size.x* t->size.x)
+
+		for ( int i = 0; i < t.size.x* t.size.y* t.size.z; i++ )
+			*(t_sum->data[i]) = *(t->data[i])-*(other.data[i]);
+		return t_sub;
 	}	
 
 // "get" function takes in the coordinates of the point to be obtianed in the tensor and the tensor itslef , returning the memory add
 // of the point
-	float& get( int _x, int _y, int _z, struct tensor t )
+	float& get( int _x, int _y, int _z, struct tensor* t )
 	{	
 
 		assert( _x >= 0 && _y >= 0 && _z >= 0 );
 		assert( _x < t.data.size.x && _y < t.data.size.y && _z < t.data.size.z );
 
-		return data[
+		return t->data[
 			_z * (t.data.size.x * t.data.size.y) +
 				_y * (t.data.size.x) +
 				_x
@@ -77,30 +84,8 @@ struct tensor( int _x, int _y, int _z )
 	}
 
 
-// printing the tensor
-	static void print_tensor( struct tensor& data )
- {
-	int mx = data.size.x;
-	int my = data.size.y;
-	int mz = data.size.z;
-
-	for ( int z = 0; z < mz; z++ )
-	{  
-		printf( "[Dim%d]\n", z );
-		for ( int y = 0; y < my; y++ )
-		{
-			for ( int x = 0; x < mx; x++ )
-			{
-				printf( "%.2f \t", (float)data.get( x, y, z ) );
-			}
-			printf( "\n" );
-		}
-	}
-}
-
-
 // Loading the values of a 3D tensor into a tensor
-static tensor to_tensor( float vector_3D )
+static tensor* to_tensor( float vector_3D )
 {	
 	   //tensor in which 3D vector is to be loaded
 	int x = sizeof(vector_3D[0][0]) / sizeof(vector_3D[0][0][0]);
@@ -109,7 +94,8 @@ static tensor to_tensor( float vector_3D )
 
 	int z = sizeof(vector_3D)/y;
 
-	struct tensor t( x, y, z );  // tensor into which the array will be loaded is initialized
+	  
+	struct tensor* t =  init_tensor(x,y,z); // tensor into which the array will be loaded is initialized
 
 	for ( int i = 0; i < x; i++ )
 		for ( int j = 0; j < y; j++ )
